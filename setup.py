3d112e8e411efd5c095fc20a5718ceb5aa1df243
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-07-13 23:16:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='load-me-learn-me',
+    description='An agnostic data loading platform for the major machine learning frameworks',
+    author='Eduardo Pignatelli',
+    author_emaik='edu.pignatelli@gmail.com',
+    license='GPL',
+    packages=['lmlm']
+    )
